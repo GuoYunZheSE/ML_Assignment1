11dadb9078279f42b81dee4b5229640ba6c65f78
GuoYunZheSE <guoyunzhe.se@gmail.com>
2017-12-04 22:37:05 +0800

    12.4 B8

--- ML_lab1_linear_regression.py
+++ ML_lab1_linear_regression.py
@@ -11,11 +11,30 @@ def Grad(m_lambda,W,train_X):
     return grad
 
 
+def Draw(loops,train_loss,validation_loss):
+    #the first 100loops
+    plt.plot(np.arange(0,100,1), train_loss[0:100], label='Train Loss')
+    plt.plot(np.arange(0,100,1), validation_loss[0:100], label='Validation Loss')
+    plt.xlabel('loops')
+    plt.ylabel('loss')
+    plt.title('The First 100 loops')
+    plt.legend()
+    plt.show()
+    #the last 10000 loops
+    plt.plot(np.arange(loops-9999, loops-1, 1), train_loss[loops-9999:loops-1], label='Train Loss')
+    plt.plot(np.arange(loops-9999, loops-1, 1), validation_loss[loops-9999:loops-1], label='Validation Loss')
+    plt.xlabel('loops')
+    plt.ylabel('loss')
+    plt.title('The Last 10000 loops')
+    plt.legend()
+    plt.show()
+
+
 if __name__ == '__main__':
     # Cost Function:0.5*lambda*W'*W+0.5*(Y-X*W)'*(Y-X*W)
 
     # Read Data
-    Data_Path='G:\\2017\\VS2017\\ML_Assignment1\\ML_Assignment1\\DataSet\\housing.txt'
+    Data_Path='/home/lucas/Codes/GitHub/ML_Assignment1/ML_Assignment1/DataSet/housing.txt'
     Data_Parameter,Data_Value=load_svmlight_file(Data_Path)
     Data_Parameter=Data_Parameter.toarray()
     train_X, val_X,train_Y,val_Y = train_test_split(Data_Parameter,Data_Value,test_size=0.3, random_state=1)
@@ -34,12 +53,14 @@ if __name__ == '__main__':
 
 
     # BGD
-    max_loop = 100000  # in case it won't converage
+    max_loop = 100000 # in case it won't converage
     epsilon = 0.000001
     count = 0
     error = np.zeros((col, 1))
     finish = 0
     Tensor = np.dot(-train_X.transpose(), train_Y)  # It never change during our process, so I put it in a tensor
+    TL=[]
+    VL=[]
     while count <= max_loop:
         count += 1
         W = W - N * (Grad(m_lambda,W,train_X) + Tensor)
@@ -50,6 +71,9 @@ if __name__ == '__main__':
             error = W
             Loss_Train=0.5*m_lambda*W.transpose().dot(W)+0.5*(train_Y-train_X.dot(W)).transpose().dot((train_Y-train_X.dot(W)))
             Loss_Validation=0.5*m_lambda*W.transpose().dot(W)+0.5*(val_Y-val_X.dot(W)).transpose().dot((val_Y-val_X.dot(W)))
-            print('Loop {}'.format(count),'Loss_Train: ',Loss_Train,'Loss_Validation: ', Loss_Validation)
+            TL.append(Loss_Train[0]/t_row)
+            VL.append(Loss_Validation[0]/v_row)
+            print('Loop {}'.format(count),'Loss_Train: ',Loss_Train/t_row,'Loss_Validation: ', Loss_Validation/v_row)
             # print(count)  #You can choose whether to print Count/W
             # print(W)
+    Draw(count,TL,VL)
